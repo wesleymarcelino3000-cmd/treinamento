fc717ce6b78b66fe06fe5d4894735c8730009383
wesleymarcelino3000-cmd <wesleymarcelino3000@gmail.com>
2026-05-06 18:27:12 -0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,1 @@
-const CACHE = 'ppt-saas-premium-v2';
-const ASSETS = ['/', '/manifest.webmanifest', '/icons/icon.svg', '/splash.svg'];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
-});
-self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
-});
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then(res => res || caches.match('/'))));
-});
+self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>self.clients.claim());
